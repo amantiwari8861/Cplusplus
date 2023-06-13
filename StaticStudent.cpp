6a99daf02d5e4c8e199b81db10744b50b9d47038
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-06-13 11:36:00 +0530

    strings and static

--- StaticStudent.cpp
+++ StaticStudent.cpp
@@ -6,7 +6,11 @@ class Student
         static int rollno;
         string stuname;
         static string schoolname;
+        // static const char address[50]={"noida 62"}; //error
+        // static const string address={"noida 62"}; //error
+        static const int pincode=201301;
         static void incrementRollNo();
+        void greet();
     public:
         Student(){}
         Student(string stuname)
@@ -18,10 +22,17 @@ class Student
         {
             cout<<"==========Student Details=========="<<endl;
             cout<<"Roll No :"<<Student::rollno<<endl;
+            cout<<"Roll No :"<<rollno<<endl;
             cout<<"Name :"<<stuname<<endl;
             cout<<"School Name :"<<Student::schoolname<<endl;
+            cout<<"School Pincode :"<<pincode<<endl;
+            greet();
         }
 };
+// void Student::greet()
+// {
+//     cout<<"helo";
+// }
 string Student::schoolname="Dps";
 int Student::rollno=0;
 void Student::incrementRollNo()
