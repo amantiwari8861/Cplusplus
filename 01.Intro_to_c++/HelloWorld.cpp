be546dd686e4239f0037b5d46197b00db50f44af
Aman Tiwari <amantiwari2743@gmail.com>
2023-06-20 11:59:22 +0530

    crlf changed to lf

---
+++ 01.Intro_to_c++/HelloWorld.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std; //try toggling this comment
+
+int main()
+{
+    // std::cout<<"\nHello World \n";
+
+    // cout<<"Hello World 2.0\n";
+
+    cout<<"Hii 1"<<endl;
+    cout<<"Hii 1"<<endl;
+    cout<<"Hii 1"<<endl;
+    cout<<"Hii 1"<<endl;
+    cout<<"Hii 1"<<endl;
+    cout<<"Hii 1"<<endl;
+
+    int a=10,b=20,c;
+    c=a+b;
+    cout<<"the value of a is "<<a<<" . anything \n"<<endl; 
+
+    cout<<"the sum of "<<a<<" and "<<b<<" is "<<c;
+    cout<<"\n";
+    return 0;
+}
---
+++ 01.Intro_to_c++/volumeOfSphere.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // 4/3*pi*r*r*r
+
+    // float r,pi=3.14,result;
+    // cout<<"enter the radius :";
+    // cin>>r;
+
+    // result=4.0/3.0*pi*r*r*r;
+
+    // cout<<"the volume of sphere is "<<result<<"\n";
+
+    int arr[5]={10,46,7,78};
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<arr[i]<<"\n";
+    }
+
+    return 0;
+}
